8a67fbae4391850f72bbc3ce64c1a60ec027114d
andriy-demeshko <ande777complaint@gmail.com>
2023-05-23 22:07:50 +0300

    Implement a read serializer with detailed book info

---
+++ borrowing/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from books.serializers import BookSerializer
+from borrowing.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
+class BorrowingReadSerializer(BorrowingSerializer):
+    book = BookSerializer(many=False, read_only=False)
