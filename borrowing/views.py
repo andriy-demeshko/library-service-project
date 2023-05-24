7be41c61635e9b74c16d2c73cb674e24e8f9a3db
andriy-demeshko <ande777complaint@gmail.com>
2023-05-24 22:56:38 +0300

    Attach the current user to the borrowing & implement create endpoint

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,9 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingReadSerializer
+from borrowing.serializers import (
+    BorrowingReadSerializer,
+    BorrowingCreateSerializer,
+)
 
 
-class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Borrowing.objects.select_related("book", "user")
-    serializer_class = BorrowingReadSerializer
+
+    # def get_queryset(self):
+    #     return Borrowing.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return BorrowingReadSerializer
+
+        elif self.action == "create":
+            return BorrowingCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
