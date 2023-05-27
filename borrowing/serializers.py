9ddf3651cd33416f2168b032f38cf40d18cf7e68
andriy-demeshko <ande777complaint@gmail.com>
2023-05-27 17:48:44 +0300

    Fix some bugs found as a result of tests

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -5,19 +5,6 @@ from books.serializers import BookSerializer
 from borrowing.models import Borrowing
 
 
-# class BorrowingSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Borrowing
-#         fields = (
-#             "id",
-#             "borrow_date",
-#             "expected_return_date",
-#             "actual_return_date",
-#             "book",
-#             "user",
-#         )
-
-
 class BorrowingReadSerializer(serializers.ModelSerializer):
     book = BookSerializer(many=False, read_only=True)
 
